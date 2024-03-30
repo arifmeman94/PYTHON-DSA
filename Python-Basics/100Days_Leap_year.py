4ecda8be20e6cd731873931785a4c54725501bcb
Arif Meman <arifmeman94@gmail.com>
2024-03-30 04:33:57 +0000

    pt

--- Python-Basics/100Days_Leap_year.py
+++ Python-Basics/100Days_Leap_year.py
@@ -1,6 +1,6 @@
 if N%4 == 0 and N%100 == 0 and N%400==0:
-            return 1
-        elif N%4 == 0 and (N%100 != 0 or N%400 ==0):
-            return 1
-        else:
-            return 0
+    return 1
+elif N%4 == 0 and (N%100 != 0 or N%400 ==0):
+    return 1
+else:
+    return 0
